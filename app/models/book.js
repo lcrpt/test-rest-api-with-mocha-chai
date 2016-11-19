d9e9cdfd3d985769520e9b87f51bec01fc5963c7
leolassence <leolassence@gmail.com>
2016-11-19 15:27:19 +0100

    added books collection schema with mongoose

---
+++ app/models/book.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    year: { type: Number, required: true },
+    pages: { type: Number, required: true, min: 1 },
+    createdAt: { type: Date, default: Date.now },
+  },
+  { versionKey: false },
+);
+
+BookSchema.pre('save', next => {
+  const now = new Date();
+
+  if (!this.createdAt) {
+    this.createdAt = now;
+  }
+
+  next();
+});
+
+export default mongoose.model('book', BookSchema);
